764633824a50805dc76dd43115f31e18e2c84ac1
salaydin <salihtalha19991@gmail.com>
2022-05-10 18:04:25 -0500

    msg

--- JS/Basics/script.js
+++ JS/Basics/script.js
@@ -2,5 +2,22 @@ console.log("Hello and bye");
 console.error("this is an error");
 console.dir("/path/");
 
-// document.write("Hello");
-alert("ok or not?")
+document.write("Hello");
+// alert("ok or not?"
+
+
+
+var school = "TG";
+var age1 = 25;
+var age2 = 46;
+
+var age3 = "25";
+var age4 = "46";
+
+console.log(school);
+console.log(age1);
+console.log(age2);
+console.log(age1 + age2);
+console.log(age3 + age4);
+
+document.write(" " + age1 + " " + age2);
